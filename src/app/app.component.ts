0a06eb2a559e0fd39f6afe25b14c491338f64b4a
Maksym Malyshko <maksymmalyshko@Maksyms-MacBook-Pro.local>
2019-12-28 17:54:31 +0200

    create datepicker with date-fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as _moment from "moment";
+import { default as _rollupMoment, Moment } from "moment";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatDatepicker } from "@angular/material/datepicker";
+
+const moment = _rollupMoment || _moment;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'dateApp';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      date: new FormControl(moment(new Date()), Validators.required)
+    });
+    console.log(this.form.value);
+  }
+
+  chosenYearHandler(normalizedYear: Moment) {
+    const ctrlValue = this.form.controls.date.value;
+    ctrlValue.year(normalizedYear.year());
+    this.form.controls.date.setValue(ctrlValue);
+  }
+
+  chosenMonthHandler(normalizedMonth: Moment) {
+    const ctrlValue = this.form.controls.date.value;
+    ctrlValue.month(normalizedMonth.month());
+    this.form.controls.date.setValue(ctrlValue);
+  }
+  chosenDayHandler(normalizedDay: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.form.controls.date.value;
+    ctrlValue.day(normalizedDay.day());
+    this.form.controls.date.setValue(ctrlValue);
+    datepicker.close();
+  }
+  submit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS
+} from "@angular/material-moment-adapter";
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 
-import { AppComponent } from './app.component';
+export const MY_FORMATS = {
+  parse: {
+    dateInput: "DD/MM/YYYY"
+  },
+  display: {
+    dateInput: "DD/MM/YYYY",
+    monthYearLabel: "MMM YYYY",
+    dateA11yLabel: "LL",
+    monthYearA11yLabel: "MMMM YYYY"
+  }
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+    { provide: MAT_DATE_LOCALE, useValue: "en-GB" }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
